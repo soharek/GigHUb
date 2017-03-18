6380ffd241f4bbc5170ab1ff2e5403401ec56b72
Witold Stafiej <soharek@gmail.com>
2017-03-18 17:53:17 +0100

    Adding gig to Databse -added - still need some refactoring

--- GigHub/Controllers/GigsController.cs
+++ GigHub/Controllers/GigsController.cs
@@ -1,5 +1,7 @@
 ﻿using GigHub.Models;
 using GigHub.ViewModels;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Linq;
 using System.Web.Mvc;
 
@@ -15,6 +17,7 @@ namespace GigHub.Controllers
         }
 
        
+        [Authorize]
         public ActionResult Create()
         {
             var viewModel = new GigFormViewModel
@@ -23,6 +26,31 @@ namespace GigHub.Controllers
             };
 
             return View(viewModel);
+        }
+
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult Create(GigFormViewModel viewModel)
+        {
+            var artitstID = User.Identity.GetUserId(); //i take out users Id
+            var artist = _context.Users.Single(u => u.Id == artitstID); // here i use the Id from line above
+            var genre = _context.Genres.Single(g => g.Id == viewModel.Genre);
+
+            var gig = new Gig()
+            {
+                Artist = artist,
+                Venue = viewModel.Venue,
+                Genre = genre,
+                DateTime = DateTime.Parse($"{viewModel.Date} {viewModel.Time}")
+
+            };
+
+            _context.Gigs.Add(gig);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+
         }
     }
 }
